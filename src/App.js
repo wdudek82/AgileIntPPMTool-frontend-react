1696104c9ead466743cea8fa7e4cb000d0984dbb
Wojciech Dudek <wdudek82@gmail.com>
2018-11-24 19:25:25 +0100

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddProject from './components/Project/AddProject';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
